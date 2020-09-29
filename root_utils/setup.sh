0da824fa0f3b7179ac08345f660a186e095403cd
Lukas Berns <lukas.berns@hep.phys.titech.ac.jp>
2020-09-30 03:12:40 +0900

    Variables weren't exported

--- root_utils/setup.sh
+++ root_utils/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-HDF5ROOT="${PWD}/../../hdf5-1.10.5"
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${HDF5ROOT}/src/.libs:${HDF5ROOT}/c++/src/.libs"
+export HDF5ROOT="${PWD}/../../hdf5-1.10.5"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${HDF5ROOT}/src/.libs:${HDF5ROOT}/c++/src/.libs"
+
+export WCSIMLIB="$HOME/watchmal/WCSim_build"
+export WCSIMDIR="$HOME/watchmal/WCSim"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${WCSIMLIB}"
