127def81372fdfc1aeb9b942bbcaa6a2b5ba0ccb
Lukas Berns <lukas.berns@hep.phys.titech.ac.jp>
2021-01-24 21:41:47 +0900

    Multi-head learning

---
+++ root_utils/notebooks/scripts/resnet_mh_geom.py
@@ -0,0 +1,136 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from common import *
+from resnet_geom import BasicGeomBlock
+
+# based on https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
+
+# this is a multi-head version of ResetGeom
+
+class ResNetMHGeom(nn.Module):
+    def __init__(self, block, num_blocks, head_num_classes=[3], overall_in_planes=8, overall_ch=64, initial_stride=[2,2], use_layer0=False):
+        """
+        head_num_classes: an array with an integer for each head
+                          where the integer gives the number of classes
+                          for the given head. The final output will
+                          simply concatenate the outputs from each head,
+                          so e.g. [1,3,3] with ResNetMHGeom gives the 
+                          same output as ResNetGeom with 7 output values,
+                          but each head has its own fully-connected layer.
+        """
+        # overall_in_planes: 8 for Q+T+3PMTpos+3PMTdir
+        # for mPMT_as_layers: 44 for 19Q+19T+3PMTpos+3PMTdir
+
+        # for mPMT_as_layers use initial_stride=(1,1)
+        # and use_layer0 = True
+        super(ResNetMHGeom, self).__init__()
+        self.pad = 1
+        
+        # (1,126,126)
+        # ( , 29, 29) for mPMT_as_layers
+        self.conv1 = nn.Conv2d(overall_in_planes, overall_ch, kernel_size=2*self.pad+1,
+                               stride=initial_stride[0], padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(overall_ch)
+        # outsize = floor[(126+2*self.pad-3)/stride + 1]
+        # so for stride=1 we have 126,
+        #              =2          63 = floor(63.5)
+        # the padding is done by geometricPad
+        # thus (64,63,63)
+        #      (64,29,29) for mPMT_as_layers with initial_stride=(1,)
+        self.conv2 = nn.Conv2d(overall_ch, overall_ch, kernel_size=2*self.pad+1,
+                               stride=initial_stride[1], padding=0, bias=False)
+        self.bn2 = nn.BatchNorm2d(overall_ch)
+        # outsize = floor[(63+2*self.pad-3)/stride + 1]
+        # so for stride=1 we have 63,
+        #              =2         32
+        # thus (64,32,32)
+        #      (64,29,29) for mPMT_as_layers with initial_stride=(1,1)
+
+        self.in_planes = overall_ch # match with output of bn2
+        self.layer0 = None
+        if use_layer0:
+            self.layer1 = block(overall_ch,overall_ch)
+        self.layer1 = self._make_layer(block,   overall_ch, num_blocks[0], stride=(2 if use_layer0 else 1))
+        #      (64,32,32)
+        #      (64,15,15) for mPMT_as_layers with use_layer0
+        self.layer2 = self._make_layer(block,   overall_ch, num_blocks[1], stride=2)
+        #      (64,16,16)
+        #      (64, 8, 8) for mPMT_as_layers
+        self.layer3 = self._make_layer(block,   overall_ch, num_blocks[2], stride=2)
+        #      (64, 8, 8)
+        #      (64, 4, 4) for mPMT_as_layers
+        self.layer4 = self._make_layer(block, 2*overall_ch, num_blocks[3], stride=2)
+        #      (128,2, 2)
+        flattening_kernel_size = 4
+        if use_layer0:
+            flattening_kernel_size = 2
+        self.convbn5 = nn.Sequential(
+            nn.Conv2d(2*overall_ch,2*overall_ch,kernel_size=flattening_kernel_size,stride=1,padding=0),
+            nn.BatchNorm2d(2*overall_ch)
+        )
+        #      (128,1, 1)
+        # transform to 128, then
+        heads = []
+        for num_classes in head_num_classes:
+            heads.append(nn.Sequential(
+                nn.Linear(int(2*overall_ch*block.expansion),   int(2*overall_ch*block.expansion/2)),
+                nn.ReLU(),
+                nn.Linear(int(2*overall_ch*block.expansion/2), int(2*overall_ch*block.expansion/4)),
+                nn.ReLU(),
+                nn.Linear(int(2*overall_ch*block.expansion/4), int(2*overall_ch*block.expansion/8)),
+                nn.ReLU(),
+                nn.Linear(int(2*overall_ch*block.expansion/8), num_classes)
+            ))
+        self.heads = nn.ModuleList(heads)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(geometricPad(x,self.pad))))
+        out = F.relu(self.bn2(self.conv2(geometricPad(out,self.pad))))
+        if self.layer0 is not None:
+            out = self.layer0(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.relu(self.convbn5(out))
+        #out = F.avg_pool2d(out, out.shape[2:4])
+        out = out.view(out.size(0), -1)
+        return torch.cat([head(out) for head in self.heads],1)
+
+#    # I'm not sure why I need to implement cpu/cuda myself, train works somehow
+#    
+#    def cpu(self):
+#        super(GeomCNN, self).cpu()
+#        for module in self._feature:
+#            module.cpu()
+#        for module in self._classifier:
+#            module.cpu()
+#        return self
+#    
+#    def cuda(self):
+#        super(GeomCNN, self).cuda()
+#        for module in self._feature:
+#            module.cuda()
+#        for module in self._classifier:
+#            module.cuda()
+#        return self
+
+def ResNetMHGeom18(head_num_classes):
+    """
+    head_num_classes: an array with an integer for each head
+                      where the integer gives the number of classes
+                      for the given head. The final output will
+                      simply concatenate the outputs from each head,
+                      so e.g. [1,3,3] with ResNetMHGeom gives the 
+                      same output as ResNetGeom with 7 output values,
+                      but each head has its own fully-connected layer.
+    """
+    return ResNetMHGeom(BasicGeomBlock, [2, 2, 2, 2], head_num_classes)
